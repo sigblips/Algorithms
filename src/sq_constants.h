d7f990fa711c627f6d9aaa2ea07db161f6131b01
Aditya Bhatt <adityabhatt@acm.org>
2011-05-09 19:00:57 +0530

    Initial steps towards library-fication. Defined the library name as setikit, the linker will take -lsetikit as a linker option. This is now required to compile the blocks.

---
+++ src/sq_constants.h
@@ -0,0 +1,11 @@
+#ifndef SQ_CONSTANTS_H
+#define SQ_CONSTANTS_H
+
+//Error codes
+const int   err_arg_bounds      = -1,
+            err_stream_open     = -2,
+            err_stream_close    = -3,
+            err_stream_read     = -4,
+            err_stream_write    = -5;
+
+#endif
